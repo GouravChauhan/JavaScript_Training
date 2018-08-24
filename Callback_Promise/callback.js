595d1f2dd2859842e1ca2a968aebd4cc82cbfb95
TFTUS\chauhan.gourav <chauhan.gourav@tftus.com>
2018-08-24 17:35:53 +0530

    Callback_Promise_DoneTest

---
+++ Callback_Promise/callback.js
@@ -0,0 +1,13 @@
+var request = require('request');
+var myCallback = function(url,cb){
+    request(url,function (error,response,body) {
+		cb(error,response,body);
+	});
+}
+
+cb = function(error,response,body){
+	console.log(response.statusCode);
+	console.log(response.body);
+}
+
+myCallback('https://www.google.co.in/',cb);
---
+++ Callback_Promise/promise.js
@@ -0,0 +1,23 @@
+var request = require('request');
+var myPromise = function(url){
+    return new Promise (function(resolve,reject){
+        request(url,function (error,response,body){
+            if(error){
+                reject(error);
+            }
+            else{
+                resolve(response);
+            }
+        });
+    });
+}
+
+myPromise('htts://www.google.co.in/')
+    .then(function(response){
+        console.log("Code : " + response.statusCode)
+        },
+        function(error){
+            console.log("Error123: "+ error)
+        }
+)
+
